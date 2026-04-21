9564a6131af7e5f606e9a5394d835510cbc51927
Bastien Teixeira <bastien.teixeira38@gmail.com>
2026-04-21 17:25:09 +0200

    ✨ Add missing Call Queue fields

--- src/Entities/SmartRoutings/CallQueue.php
+++ src/Entities/SmartRoutings/CallQueue.php
@@ -12,6 +12,11 @@ class CallQueue extends Entity
         public ?string $number = null,
         public ?int $call_queue_group_id = null,
         public ?int $customer_id = null,
+        public ?string $host_name = null,
+        public ?string $type = null,
+        public ?string $did_number = null,
+        public ?string $default_destination_type = null,
+        public ?string $default_destination = null,
     ) {
         //
     }
